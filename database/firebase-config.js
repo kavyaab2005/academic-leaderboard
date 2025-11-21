4d2b860e718e083d1f229089921abe4ea412c9bc
kavyaab2005 <kavya0308181@gmail.com>
2025-11-21 12:55:26 +0530

    Create firebase-config.js

---
+++ database/firebase-config.js
@@ -0,0 +1,6 @@
+// database/firebase-config.js
+// =============================================
+const admin = require('firebase-admin');
+const serviceAccount = require('./firebase-key.json');
+admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
+module.exports = admin;
