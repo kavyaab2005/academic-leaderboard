3379fe8c770db3faa467c160a342852ec2d7b264
kavyaab2005 <kavya0308181@gmail.com>
2025-11-21 12:52:53 +0530

    Create MySQL schema for students table

---
+++ database/mysql.sql
@@ -0,0 +1,9 @@
+// =============================================
+// database/mysql.sql
+// =============================================
+-- MySQL schema file
+CREATE TABLE students (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100),
+usn VARCHAR(20)
+);
