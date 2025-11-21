467f3a27d174d3ef3ea8dc0e90805d0be8b7a3bf
kavyaab2005 <kavya0308181@gmail.com>
2025-11-21 12:39:44 +0530

    Create studentRoutes.js

---
+++ backend/routes/studentRoutes.js
@@ -0,0 +1,13 @@
+// =============================================
+// routes/studentRoutes.js
+// =============================================
+const express = require('express');
+const router = express.Router();
+const studentController = require('../controllers/studentController');
+
+
+router.get('/', studentController.getStudents);
+router.post('/add', studentController.addStudent);
+
+
+module.exports = router;
