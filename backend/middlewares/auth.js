b4ecf40e81a481a39711eebad737e755bf618e6b
kavyaab2005 <kavya0308181@gmail.com>
2025-11-24 14:03:30 +0530

    Delete backend directory

--- backend/middlewares/auth.js
+++
@@ -1,7 +0,0 @@
-// middlewares/auth.js
-// =============================================
-module.exports = (req, res, next) => {
-const token = req.headers.authorization;
-if (token === 'ADMIN-TOKEN-123') next();
-else res.json({ message: 'Unauthorized' });
-};
--- backend/models/admin.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("../database/mongo");
-const AdminSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String
-});
-module.exports = mongoose.model("Admin", AdminSchema);
